5d362e331fd0775d378f72b9f9438b2d8482d581
leedavi <dcl@agence-sesame.fr>
2022-03-23 06:08:42 +0100

    scheduler

---
+++ Components/Scheduler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DNNrocketAPI;
+using DNNrocketAPI.Components;
+using RocketCatalog.Components;
+using Simplisity;
+
+namespace RocketDocs.Components
+{
+    public class Scheduler : SchedulerInterface
+    {
+        /// <summary>
+        /// This is called by DNNrocketAPI.Components.RocketScheduler
+        /// </summary>
+        /// <param name="systemData"></param>
+        /// <param name="rocketInterface"></param>
+        public override void DoWork()
+        {
+            var portalList = PortalUtils.GetPortals();
+            foreach (var portalId in portalList)
+            {
+                var portalCatalog = new PortalCatalogLimpet(portalId, DNNrocketUtils.GetCurrentCulture());
+
+                if (portalCatalog.Active && (portalCatalog.SchedulerRunHours == 0 || (portalCatalog.LastSchedulerTime < DateTime.Now.AddHours(portalCatalog.SchedulerRunHours * -1))))
+                {
+
+                    portalCatalog.LastSchedulerTime = DateTime.Now;
+                    portalCatalog.Update();
+                }
+                else
+                {
+                    if (portalCatalog.DebugMode) LogUtils.LogSystem("RocketCatalog Scheduler not run, LastSchedulerTime: " + portalCatalog.LastSchedulerTime.ToString("O") + " CurrentTime: " + DateTime.Now.ToString("O"));
+                }
+            }
+        }
+    }
+}
